a1aa02c2549c39df20e756887cde61b201264a37
isaac8069 <isaac8069@gmail.com>
2021-11-29 17:16:11 -0500

    React To Do List complete

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyList from './MyList';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const toDos = ["More wine", "Drink more water", "Read over documents from old SEI lessons", "Sleep at 11PM", "Wake up at 6:15AM to workout Monday thru Friday", "No more eating meat", "Read every night"]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyList theList={toDos} />,
   document.getElementById('root')
-);
+)
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
