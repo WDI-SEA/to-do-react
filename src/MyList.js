6d44ed7c754b3b65ded90239f863331b15d9bfc6
Emily Barwinczak <ebarw@macbook-air.home>
2021-11-29 17:23:00 -0500

    deliverable

---
+++ src/MyList.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import './App.css'
+import ListItem from './ListItem'
+
+class MyList extends Component {
+  render() {
+    let todoItems = this.props.theList.map((item, index) => {
+      return <ListItem task={item} key={index} />
+    })
+    
+    return (
+      <div>
+        <h1>Things I should stop procrastinating:</h1>
+        <ul>
+          {todoItems}
+        </ul>
+      </div>
+    )
+  }
+}
+
+
+export default MyList
