4df9a0e01a2be00b14fe3a08360c97da4f393faf
steven43533 <steven43533@gmail.com>
2021-11-29 17:04:08 -0500

    Finished requirements

---
+++ src/ListItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class ListItem extends Component {
+    render() {
+        return(
+            <div>
+                <li>{this.props.task}</li>
+            </div>
+        )
+    }
+}
+
+
+export default ListItem
---
+++ src/MyList.js
@@ -0,0 +1,45 @@
+// import logo from './logo.svg';
+// import './App.css';
+
+// function MyList() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default MyList;
+
+import React, { Component } from 'react'
+import './App.css'
+import ListItem from './ListItem'
+
+class MyList extends Component {
+  render() {
+    let todoItems = this.props.theList.map((item, index) => (<ListItem task={item} key={index} />))
+    return (
+      <div>
+        <h1> Things I should stop procrastinating</h1>
+        <ul>
+          {todoItems}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default MyList
