bd2141ea9e1bdddb3be5a15be433799c97273666
jyang1003git config --global user.name <jerryyang1003@gmail.com>
2021-11-30 09:14:27 -0500

    todo homework

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyList from './MyList';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+const toDos = ["Listen to Hot Mulligan", "Drink hot cocoa", "Eat hot cheetos"]
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyList theList={toDos} />,
   document.getElementById('root')
-);
+)
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
