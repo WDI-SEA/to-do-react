cc31215d1cc1d80a80706056e1f5acf6af6feefb
harrdev <jasonharrphx@live.com>
2021-11-30 21:12:41 -0700

    updated

--- src/MyList.js
+++ src/MyList.js
@@ -3,16 +3,54 @@ import './App.css'
 import ListItem from './ListItem'
 
 class MyList extends Component {
-  
+
+  state = {
+    taskArray: this.props.theList,
+    newItem: ''
+  }
+
+  clearList = () => {
+    console.log("clearing list")
+    this.setState({
+      taskArray: []
+    })
+  }
+
+  newItemChange = (e) =>{
+    this.setState({newItem: e.target.value})
+  }
+
+  addItem = (e) => {
+    e.preventDefault()
+    let tempTaskArray = this.state.taskArray
+    tempTaskArray.push(this.state.newItem)
+    this.setState({
+      taskArray: tempTaskArray,
+      newItem: ''
+    })
+  }
+
   render() {
-    let todoItems = this.props.theList.map((item, index) => (
-      <ListItem task={item} key={index} />
-    ))
-    return(<div className="listDiv">
-      <h1>Things I should stop procrastinating:</h1><hr></hr>
+
+    let todoItems = this.state.taskArray.map((item, index) => {
+      return <ListItem task={item} key={`todo${index}`} />
+    })
+
+    return(<div>
+      <h1>Things I should stop procrastinating:</h1>
+      <form>
+        <input 
+          type="text" 
+          placeholder="type an item here"
+          onChange={(e)=>this.newItemChange(e)}
+          value={this.state.newItem}
+        />
+        <button onClick={(e)=>this.addItem(e)}>Add It!</button>
+      </form>
       <ul>
         {todoItems}
       </ul>
+      <button onClick={this.clearList}>Clear List</button>
     </div>)
   }
 }
