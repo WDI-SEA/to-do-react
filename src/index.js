06bbd30fa309d6c413274090202b8cf8a9875c4d
Norman Teodoro <normanteodoro@hotmail.com>
2022-03-08 19:49:24 -0800

    deliverable norman

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MyList from './MyList';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const toDos = ["Read", "Study", "Study some more"]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyList theList={toDos} />,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
