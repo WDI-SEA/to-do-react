25bdc5f38fdfe3e449ef904ae091c9bbb871e500
kokopuffz <chanheeya@gmail.com>
2022-03-08 23:23:17 -0600

    show task from listitem comp

--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -1,14 +1,14 @@
-import React, { Component } from "react";
-import "./App.css";
+import { Component } from 'react';
 
-class ListItem extends Component {
-  render() {
+export default class ListItem extends Component {
+  state = {  } 
+  render() { 
     return (
       <div>
         <li>{this.props.task}</li>
       </div>
-    );
+    )
   }
 }
+ 
 
-export default ListItem;
--- src/MyList.jsx
+++ src/MyList.jsx
@@ -1,12 +1,15 @@
 import { Component } from 'react';
 import './App.css'
+import ListItem from "./ListItem";
 
 export default class MyList extends Component {
   render() {
     return (
       <div>
         <h1>Things I should stop procrastinating:</h1>
-        <li></li>
+        <ul>
+          <ListItem task="Buy ice cream"/>
+        </ul>
       </div>
     )
   }
