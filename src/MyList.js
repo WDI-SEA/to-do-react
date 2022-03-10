05cac1f167afc5e3bcff0679b2b2bfa7119f3447
romanprotoliuk <romanprotoliuk@gmail.com>
2022-03-09 16:31:38 -0800

    Form, state

--- src/MyList.js
+++ src/MyList.js
@@ -4,14 +4,53 @@ import { Component } from 'react';
 import ListItem from './ListItem';
 
 class MyList extends Component {
+	state = {
+		taskArray: this.props.theList,
+		newItem: ''
+	};
+
+	clearList = () => {
+		console.log('Clearing list');
+		this.setState({ taskArray: [] });
+	};
+
+	addItem = (e) => {
+		e.preventDefault();
+		// push the new task into the task array
+		console.log('New item form has been submitted');
+		let newList = this.state.taskArray;
+		newList.push(this.state.newItem);
+		this.setState({
+			taskArray: newList,
+			newItem: ''
+		});
+	};
+
+	handleChange = (e) => {
+		this.setState({ newItem: e.target.value });
+	};
+
 	render() {
-		let todoItems = this.props.theList.map((item, index) => {
+		let todoItems = this.state.taskArray.map((item, index) => {
 			return <ListItem task={item} key={index} />;
 		});
 		return (
 			<div>
 				<h1>Things I should stop procrastinating:</h1>
+				<form onSubmit={this.addItem}>
+					<label htmlFor="newItem">Add a new task:</label>
+					<input
+						type="text"
+						id="newItem"
+						name="newItem"
+						onChange={this.handleChange}
+						value={this.state.newItem}
+					/>
+					<button type="submit"> Submit</button>
+				</form>
+
 				<ul>{todoItems}</ul>
+				<button onClick={this.clearList}>Clear All Items</button>
 			</div>
 		);
 	}
